52e993c4b211c6f6952f10efd8c30306969a0bd1
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-05-29 10:24:26 -0500

    move dist to docs

--- scripts/start.js
+++ scripts/start.js
@@ -14,7 +14,7 @@ concurrently(
             prefixColor: "bgBlue.bold",
         },
         {
-            command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
+            command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 docs -w --no-online`,
             name: "SB_BROWSER_SYNC",
             prefixColor: "bgGreen.bold",
         },
